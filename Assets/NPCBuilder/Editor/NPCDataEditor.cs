5178548476182764c32c1e8e4372150f20bfecd8
DESKTOP-9MUID15\Scott Melanson <scott.melanson3@gmail.com>
2020-11-13 18:22:00 -0500

    Created an editor window for NPCBodyPart

---
+++ Assets/NPCBuilder/Editor/NPCDataEditor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+namespace TheGameMecha.NPCBuilder
+{
+    [CustomEditor(typeof(NPCBodyPart))]
+    public class NPCDataEditor : Editor
+    {
+        SerializedProperty prefab;
+        SerializedProperty bodyPartType;
+
+        Editor gameObjectEditor;
+
+        private void OnEnable()
+        {
+            prefab = serializedObject.FindProperty("bodyPartPrefab");
+            bodyPartType = serializedObject.FindProperty("bodyPartType");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(prefab);
+            EditorGUILayout.PropertyField(bodyPartType);
+
+            GUIStyle bgColor = new GUIStyle();
+            bgColor.normal.background = EditorGUIUtility.whiteTexture;
+
+            if (prefab != null)
+            {
+
+                if (gameObjectEditor == null)
+                    gameObjectEditor = Editor.CreateEditor(prefab.objectReferenceValue);
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    gameObjectEditor = Editor.CreateEditor(prefab.objectReferenceValue);
+                }
+
+                gameObjectEditor.OnInteractivePreviewGUI(GUILayoutUtility.GetRect(256, 256), bgColor);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
