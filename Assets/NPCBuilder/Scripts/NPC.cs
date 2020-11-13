2c8086da49ec696163040a470452afc7c8b6693d
DESKTOP-9MUID15\Scott Melanson <scott.melanson3@gmail.com>
2020-11-12 19:35:39 -0500

    Hooking up basic stiching with custom model

---
+++ Assets/NPCBuilder/Scripts/NPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TheGameMecha.NPCBuilder
+{
+    public class NPC : ScriptableObject
+    {
+        public GameObject head;
+        public GameObject body;
+        public GameObject pants;
+    }
+}
---
+++ Assets/NPCBuilder/Scripts/NPCBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheGameMecha.NPCBuilder
+{
+    public class NPCBuilder : MonoBehaviour
+    {
+
+    }
+}
